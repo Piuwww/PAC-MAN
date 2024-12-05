6154496fc9987b720aa3f368450141baf007796b
Clauteur <110111288+Clauteur@users.noreply.github.com>
2024-12-05 09:26:09 +0400

    Update init.py

--- init.py
+++ init.py
@@ -12,26 +12,37 @@ def clear():
         system("clear")
 
 def affiche(liste):
+    pac_l = 0
+    pac_c = 0
     for i in range (len(liste)):
-        for element in liste[i]:
-            if element == 0 :
+        for j in range (liste[i]) :
+            if labi[i][j] == 0 :
                 element =" "
-            if element == 1:
+            if labi[i][j] == 1:
                 element ="□"
-            if element == 2 :
+            if labi[i][j] == 2 :
                 element ="ᗧ"
+                pac_l = j
+                pac_c = i
             print (element, end=" ")
         print()
     time.sleep(1/30)
     system("cls")
-    return ""
+    return i , j
 
 def aski():
     if kbhit():
         z = getch() #lecture touche
         code  = ord(z) # code ASCII
 		
-#def mouvement(aski):
+def mouvement(aski):
+    i , j = affiche(labi)
+    if aski == 122 :
+        labi[i][j]
+    if aski == 121 :
+        labi[i][j]
+
+
     
 
 #init
