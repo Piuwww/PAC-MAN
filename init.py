1e7ec566d6905b71a570b0ea4160523decbd269a
Clauteur <110111288+Clauteur@users.noreply.github.com>
2024-12-05 09:06:14 +0400

    Update init.py

--- init.py
+++ init.py
@@ -3,6 +3,7 @@ from os import system
 from random import randint
 from sys import platform
 import time
+from msvcrt import getch,kbhit
 #fonction
 def clear():
     if platform.startswith("win"):
@@ -34,7 +35,7 @@ labi = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
@@ -43,5 +44,21 @@ labi = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]   
 #code
 fin=False
+j = len(labi) /2
+i = len(labi)
+while not (fin):
+    affiche(labi)
+    if kbhit():
+        z = getch() #lecture touche
+        code  = ord(z) # code ASCII
+        print (code)
+
+        if code == 122:    #bon code 122 = z mais crash
+            labi[j][i]=0
+            j = j+1
+            labi[j][i]=2
+            clear
+            affiche(labi)
+
 while not (fin):
     affiche(labi)
