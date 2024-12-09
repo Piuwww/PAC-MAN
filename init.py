49c5ef48734446eb57c5fea0fb1e4b784798b7fe
IguanaCucumber <sacha.fazzari@protonmail.com>
2024-12-09 09:10:50 +0400

    Add files via upload

--- init.py
+++ init.py
@@ -1,77 +1,63 @@
-#import
 from os import system
-from random import randint
 from sys import platform
-import time
-from msvcrt import getch,kbhit
-#fonction
+
+
 def clear():
     if platform.startswith("win"):
         system("cls")
     else:
         system("clear")
 
-def affiche(liste):
-    pac_l = 0
-    pac_c = 0
-    for i in range (len(liste)):
-        for j in range (liste[i]) :
-            if labi[i][j] == 0 :
-                element =" "
-            if labi[i][j] == 1:
-                element ="□"
-            if labi[i][j] == 2 :
-                element ="ᗧ"
-                pac_l = j
-                pac_c = i
-            print (element, end=" ")
+
+def affiche():  # Pour faire jolie
+    length = int(longueur * 1.5)
+    print()
+
+    nombre = 0
+
+    print("╭", end="")
+    print("─" * ((longueur * 2) + 1), end="")
+    print("╮", end="")
+    print()
+    for ligne in jeu:
+        nombre += 1
+        print("│ ", end="")
+        for case in ligne:
+            if case == 0:
+                print(f"{GREY}·{RESET}", end=" ")
+            elif case == 1:
+                pass
+        print("│", end="")
         print()
-    time.sleep(1/30)
-    system("cls")
-    return i , j
 
-def aski():
-    if kbhit():
-        z = getch() #lecture touche
-        code  = ord(z) # code ASCII
-        if code==122: #Z
-            return
-        if code==113: #Q
-            return
-        if code==115: #S
-            return
-        if code==100: #D
-            return
+    print("╰", end="")
+    print("─" * ((longueur * 2) + 1), end="")
+    print("╯")
 
-		
-def mouvement(aski):
-    i , j = affiche(labi)
-    if aski == 122 :
-        labi[i][j]
-    if aski == 121 :
-        labi[i][j]
+    print("\n")
 
 
-    
+def jeu_init(longueur, hauteur):
+    jeu = []
+    for i in range(longueur):
+        jeu.append([])
+        for j in range(hauteur):
+            jeu[i].append(0)
 
-#init
-labi = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-        [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]   
+    return jeu
 
 
-#code
-fin=False
-while not (fin):
-    affiche(labi)
+clear()
+
+ennemis = int(input("Nombres de fantômes (x) > 1:\n"))
+clear()
+longueur = int(input("Longueur du tableau > 2:\n"))
+clear()
+hauteur = int(input("Hauteur du tableau > 0:\n"))
+jeu = jeu_init(longueur, hauteur)
+
+YELLOW = "\033[33m"
+RED = "\033[91m"
+GREY = "\033[90m"
+BLUE = "\033[34m"
+RESET = "\033[0m"
